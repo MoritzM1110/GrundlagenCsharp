90687890251f8a43538cd69c3d905f57407002d4
Moritz Mersmann <moritz.m41@gso.schule.koeln>
2022-03-09 11:00:01 +0100

    array 1

--- Arrays_Aufgabe1/Program.cs
+++ Arrays_Aufgabe1/Program.cs
@@ -6,7 +6,39 @@ namespace Arrays_Aufgabe1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+           
+            int arbeiten = 20;
+            double summeg = 0;
+            double summe = 0;
+
+            int[] note = new int[arbeiten];
+          
+
+            for (int index = 0; index < arbeiten; index++ )
+            {
+                Console.WriteLine("Geben sie die {0} Note ein!", index + 1);
+                note[0] = Convert.ToInt32(Console.ReadLine());
+                summe = summe + note[0];
+                summeg = summe / 20;
+                
+
+            }
+
+            Console.WriteLine("Ihr Durschnitt lautet {0}", summeg) ;
+            
+
+
+            
+
+            
+
+
+
+
+
+
+
         }
     }
 }
