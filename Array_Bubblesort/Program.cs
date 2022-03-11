a933705e9db643d45a0cf4d8d9ce384f809c5c2c
Moritz Mersmann <moritz.m41@gso.schule.koeln>
2022-03-11 12:56:29 +0100

    teil

---
+++ Array_Bubblesort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Array_Bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] daten = { 45, 10, 15, 4 };
+            int n = daten.Length;
+
+
+            for (int index = 0; index < n - 1; index++)
+            {
+                Console.WriteLine(index);
+
+                if (daten[index] > daten[index + 1])
+                {
+                    double buffer = daten[index + 1];
+                    daten[index + 1] = daten[index];
+                    daten[index] = buffer;
+                }
+                else if (daten[index + 1] > daten[index + 2]) 
+                {
+                    double buffer2 = daten[index + 2];
+                    daten[index + 2] = daten[index + 1];
+                    daten[index + 1] = buffer2;
+                }
+
+                else if (daten[index + 2] > daten[index + 3]) 
+                {
+                    double buffer3 = daten[index + 3];
+                    daten[index + 3] = daten[index + 2];
+                    daten[index + 2] = buffer3;
+                }
+           
+            }
+
+
+
+
+
+        }
+    }
+}
