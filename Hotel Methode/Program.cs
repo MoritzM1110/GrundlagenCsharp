3fcc38fb2500eeeb9e950139ab5a99e84e5107bd
Moritz Mersmann <moritz.m41@gso.schule.koeln>
2022-03-30 12:51:11 +0200

    Methodenhotel aufgabe angefangen

---
+++ Hotel Methode/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Hotel_Methode
+{
+    class Program
+    {
+        static void Main()
+        {
+            int Auswahl;
+            short eingabe ;
+            short[] zimmernummer = { 45, 23, 3, 32 };
+            string[] vorname = { "John", "Paul", "George", "Ringo" };
+            string[] nachname = { "Lennon", "McCartney", "Harrison", "Starr" };
+            short[] übernachtungen = { 1, 4, 12, 3 };
+            double[] rechnungsbetrag = { 82.46, 329.84, 989.52, 247.38 };
+
+            
+            eingabe = Auswahl(zimmernummer,vorname, nachname, übernachtungen, rechnungsbetrag);
+            
+        }
+
+        static int Auswahl(short zimmernummer, string nachname)
+        {
+
+
+            return;
+
+          
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+} 
--- Methoden_Aufgabe/Program.cs
+++ Methoden_Aufgabe/Program.cs
@@ -8,71 +8,56 @@ namespace Methoden_Aufgabe
         {
 
 
+            string durchlauf;
+            string text;
+            int num;
+
+
             bool richtig = true;
             do
             {
                 richtig = true;
-                Console.WriteLine("Geben sie die Anzahl der durchläufe ein?");
-                int durchlauf = Convert.ToInt32(Console.ReadLine());
-
-                if (durchlauf < 0 & durchlauf > 1000)
+                Console.WriteLine("Geben sie bitte die Anzahl der Durchläufe ein:");
+                durchlauf = Console.ReadLine();
+                if (!Int32.TryParse(durchlauf, out num))
                 {
                     richtig = false;
-                    Console.WriteLine("Bitte geben sie eine Richtige Zahl ein!");
-                    Console.ReadLine();
+                    Console.WriteLine("Bitte geben sie eine gültige Eingabe ein. Bitte drücken sie eine beliebige Taste um es erneut zu versuchen:");
+                    Console.ReadKey();
                     Console.Clear();
-
-
-                } while (richtig == false) ;
-
-                bool richtig2 = true;
-                do
-                {
-                    richtig2 = true;
-                    Console.WriteLine("Gebn sie bitte ihre Buchstaben ein");
-                    string text = Console.ReadLine();
-
-                    if (durchlauf < 1 | durchlauf > 1000)
-                    {
-                        richtig2 = false;
-                        Console.WriteLine("Bitte geben sie eine Gültige eingabe ein!");
-                        Console.ReadKey();
-                        Console.Clear();
-                    }
-
-
-
-
-                } while (richtig2 == false);
-
-                int anzahl = 0;
-
-
-
-                int anzahl_zeichen = 0;
-
-
-                anzahl_zeichen = ausgabeTextMalX(durchlauf, text);
-                Console.Write("\nAnzahl der Buchstaben: {0}", anzahl_zeichen);
-            }
-
-        static int ausgabeTextMalX(int durchlauf, string text)
-            {
-                for (int i = 0; i < durchlauf; i++)
-                {
-                    Console.WriteLine(text);
-
-
                 }
 
-                return text.Length * durchlauf;
+            } while (richtig == false);
 
+
+
+
+
+            Console.WriteLine("Geben sie bitte ihren Text ein:");
+            text = Console.ReadLine();
+
+
+
+
+            int anzahl = 0;
+            anzahl = ausgabeTextMalX(num, text);
+
+            Console.Write("\nAnzahl der Buchstaben: {0}", anzahl);
+
+        }
+
+        static int ausgabeTextMalX(int durchlauf, string text)
+        {
+
+            for (int i = 0; i < durchlauf; i++)
+            {
+                Console.WriteLine(text);
+            }
+
+            return text.Length * durchlauf;
 
         }
 
 
     }
 }
-
-
-
