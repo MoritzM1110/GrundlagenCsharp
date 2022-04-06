cb8cb655ab68e23bf0eefc7d75263866ad855904
Moritz Mersmann <moritz.m41@gso.schule.koeln>
2022-04-06 12:54:07 +0200

    hotellogin dings

--- Hotel Methode/Program.cs
+++ Hotel Methode/Program.cs
@@ -6,27 +6,56 @@ namespace Hotel_Methode
     {
         static void Main()
         {
-            int Auswahl;
-            short eingabe ;
-            short[] zimmernummer = { 45, 23, 3, 32 };
-            string[] vorname = { "John", "Paul", "George", "Ringo" };
-            string[] nachname = { "Lennon", "McCartney", "Harrison", "Starr" };
-            short[] übernachtungen = { 1, 4, 12, 3 };
-            double[] rechnungsbetrag = { 82.46, 329.84, 989.52, 247.38 };
+            do
+            {
+                int Auswahl;
+                short eingabe_short;
+                string eingabe_string;
+                bool flag = false;
 
-            
-            eingabe = Auswahl(zimmernummer,vorname, nachname, übernachtungen, rechnungsbetrag);
-            
+                short[] zimmernummer = { 45, 23, 3, 32 };
+                string[] vorname = { "John", "Paul", "George", "Ringo" };
+                string[] nachname = { "Lennon", "McCartney", "Harrison", "Starr" };
+                short[] übernachtungen = { 1, 4, 12, 3 };
+                double[] rechnungsbetrag = { 82.46, 329.84, 989.52, 247.38 };
+
+
+
+
+                Console.WriteLine("Bitte geben sie ihre Daten ein");
+                eingabe_string = Console.ReadLine();
+                flag = short.TryParse(eingabe_string, out eingabe_short);
+
+
+
+
+                if (flag == false)
+                {
+                  var daten = (eingabe_string, zimmernummer, vorname, nachname, übernachtungen, rechnungsbetrag);
+                    
+                }
+                else
+                {
+                  var daten = (eingabe_short, zimmernummer, vorname, nachname, übernachtungen, rechnungsbetrag);
+
+                }
+
+
+
+
+            } while (true);
+
+
         }
 
-        static int Auswahl(short zimmernummer, string nachname)
-        {
 
 
-            return;
+        static (bool, short, string, string, short, double) 
+        
 
-          
-        }
+        
+
+       
         
 
 
